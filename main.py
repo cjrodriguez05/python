d8e2e164e18bca2f86e8c8fca31e09ccd0f9539b
cjrodriguez05 <112595217+cjrodriguez05@users.noreply.github.com>
2022-09-12 21:37:42 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# #variables
+# """
+# mensaje = "hola mundo, inciando python"
+
+# print(mensaje)
+
+# """
+
+
+# nombre = "julian"
+# apellido = "rodriguez"
+# ciudad = "piedecuesta"
+# celular = 3118756341
+
+# # print(nombre + " " + apellido + " " + ciudad + " " + str(celular))
+
+# print(f"hola {nombre} {apellido} de la ciudad {ciudad} con el numero {celular}")
+
+nombre = input("digita tu nombre")
+print(nombre)
+
+
+nombre = input("digita tu nombre")
+print(nombre)
