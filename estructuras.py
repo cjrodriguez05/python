5985f26ba04a7cb8a9d5c1309759503128c2b8d8
cjrodriguez05 <112595217+cjrodriguez05@users.noreply.github.com>
2022-09-14 20:56:03 -0500

    clase 14 09

---
+++ estructuras.py
@@ -0,0 +1,56 @@
+"""num = int(input("ingrese un número: \n"))
+
+if num == 100:
+    print("ud escribió el 100")
+
+elif num > 100:
+    print("ud escribió un numero mayor a 100")
+
+else:
+    print("ud escribió un numero menor a 100")"""
+
+#condicionales multiples
+
+"""x = 5
+if 0 < x:
+    if x < 10:
+        print("x es un numero positivo de un solo digito.")
+if 0 < x and x<10:
+        print("x es un numero positivo de un solo digito")
+if 0 < x <10:
+        print("x es un numero positivo de un solo digito")
+"""
+
+"""nota1= 1.0
+nota2= 2.9
+nota3= 3.8
+nota4= 1.0
+
+promedio= (nota1+nota2+nota3+nota4)/4
+
+if promedio >=3:
+    print(f"su nota fue de {promedio} , ud aprobó satisfactoriamente")
+else:
+    print(f"su nota fue de {promedio} , ud reprobó")"""
+
+
+
+"""time1= 5
+time2= 8
+
+time = int(input("ingrese un número: \n"))
+if time == 8:
+    print(f"su tiempo fue el superior")
+else:
+    print(f"puedes seguir esforzandote")
+
+    """
+
+corredor1= 2
+corredor2= 50
+
+if corredor1 < corredor2:
+    print("el ganador es el corredor1")
+else:
+    print("el ganador es el corredor2")
+
---
+++ variables.py
@@ -0,0 +1,17 @@
+#variables
+
+from operator import truediv
+
+
+suma_realizada = False
+total=0
+a=5
+b=10
+
+if (suma_realizada == False):
+    total=a+b
+    suma_realizada = True
+
+if(suma_realizada == True):
+    print("se ha realizado una suma y su valores",total)
+
